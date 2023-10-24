66aa55c85327049ccfa7ca3a7bd4f0f0f81d70fc
alalghzy <alalghzy@gmail.com>
2023-10-25 06:35:41 +0700

    ~AOS

--- resources/views/pages/user.blade.php
+++ resources/views/pages/user.blade.php
@@ -26,11 +26,11 @@
         <div class="container px-4 px-lg-5 text-center">
             <div class="row gx-4 gx-lg-5 justify-content-center">
                 <div class="col-lg-10">
-                    <h2 data-aos="flip-up" data-aos-offset="500" data-aos-duration="500">Ayoo! Flo!</h2>
+                    <h2 data-aos="flip-up" data-aos-offset="480" data-aos-duration="500">Ayoo! Flo!</h2>
                     <p class="lead mb-5" data-aos="flip-down" data-aos-offset="400" data-aos-duration="700">
                         🌱 Tertarik dengan tumbuhan 🌱
                     </p>
-                    <a class="btn btn-dark btn-xl" href="#tentang" data-aos="flip-down" data-aos-offset="200"
+                    <a class="btn btn-dark btn-xl" href="#tentang" data-aos="flip-down" data-aos-offset="400"
                         data-aos-duration="500">So pasti!</a>
                 </div>
             </div>
@@ -44,26 +44,26 @@
                 <h2 class="mb-5">Manusia dan Tumbuhan</h2>
             </div>
             <div class="row gx-4 gx-lg-5">
-                <div class="col-lg-3 col-md-6 mb-5 mb-lg-0" data-aos="flip-up" data-aos-offset="400"
-                    data-aos-duration="500">
+                <div class="col-lg-3 col-md-6 mb-5 mb-lg-0" data-aos="flip-up" data-aos-offset="300"
+                    data-aos-duration="600">
                     <span class="service-icon rounded-circle mx-auto mb-3"><i class="fa-solid fa-wind"></i></span>
                     <h4><strong>Apa hal?</strong></h4>
                     <p class="text-faded mb-0">Bukan saya!</p>
                 </div>
-                <div class="col-lg-3 col-md-6 mb-5 mb-lg-0" data-aos="flip-down" data-aos-offset="450"
-                    data-aos-duration="500">
+                <div class="col-lg-3 col-md-6 mb-5 mb-lg-0" data-aos="flip-down" data-aos-offset="340"
+                    data-aos-duration="600">
                     <span class="service-icon rounded-circle mx-auto mb-3"><i class="fa-solid fa-fan"></i></span>
                     <h4><strong>Apa gerangan?</strong></h4>
                     <p class="text-faded mb-0">Masih sama</p>
                 </div>
-                <div class="col-lg-3 col-md-6 mb-5 mb-md-0"data-aos="flip-up" data-aos-offset="500" data-aos-duration="500">
+                <div class="col-lg-3 col-md-6 mb-5 mb-md-0"data-aos="flip-up" data-aos-offset="370" data-aos-duration="600">
                     <span class="service-icon rounded-circle mx-auto mb-3"><i class="fa-solid fa-leaf"></i></span>
                     <h4><strong>Masih yang tadi</strong></h4>
                     <p class="text-faded mb-0">
                         Bukan saya lagi
                     </p>
                 </div>
-                <div class="col-lg-3 col-md-6" data-aos="flip-down" data-aos-offset="550" data-aos-duration="500">
+                <div class="col-lg-3 col-md-6" data-aos="flip-down" data-aos-offset="410" data-aos-duration="600">
                     <span class="service-icon rounded-circle mx-auto mb-3"><i class="fa-solid fa-seedling"></i></span>
                     <h4><strong>Ini saya!</strong></h4>
                     <p class="text-faded mb-0">Pasti Flora<i class="fa-solid fa-heart"></i></p>
